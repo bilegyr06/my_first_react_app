2e5785a9ad4b992db6b5872ce98eae78b40ce0d0
bilegyr06 <ayodejavu2@gmail.com>
2025-09-13 11:12:48 +0100

    beforeReactProps

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const welcome = {
+  greeting: 'hey',
+  title: 'React'
+}
+
+const list = [
+  {
+    title: ' React ', 
+    url: ' https://react.dev/ ',
+    author: 'Jordan Walke ', 
+    num_comments: 3,
+    points: 4, 
+    objectID: 0, 
+  }, 
+  {title: ' Redux ', 
+    url: ' https://redux.js.org/ ', 
+    author: ' Dan Abramov, Andrew Clark', 
+    num_comments: 2, 
+    points: 5, 
+    objectID: 1
+  }
+] 
+
+const List = ()=>(
+      <ul>
+        {list.map(function (item){
+          return <li key = {item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            </li>
+        })}
+      </ul>
+  );
+
+
+const Search = ()=>{
+  const handleChange = (event) =>{
+    console.log(event)
+
+    console.log(event.target.value);
+    
+  }
+  return(
+    <div>
+      <label htmlFor="search"> Search:</label>
+      <input type="text" id = 'search' onChange={handleChange}/>
+    </div>
+  )
+}
+
+const App = ()=>(
+    <div>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+
+      <Search />
+
+      <hr />
+      <List />
+    </div>
+
+    
+  );
+
+export default App;
