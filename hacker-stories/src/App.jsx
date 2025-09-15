e3fee75498c3759a9f792be13947e9897957a841
bilegyr06 <ayodejavu2@gmail.com>
2025-09-15 15:46:30 +0100

    Props

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,70 +2,94 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react'
 
 const welcome = {
   greeting: 'hey',
   title: 'React'
 }
 
-const list = [
-  {
-    title: ' React ', 
-    url: ' https://react.dev/ ',
-    author: 'Jordan Walke ', 
-    num_comments: 3,
-    points: 4, 
-    objectID: 0, 
-  }, 
-  {title: ' Redux ', 
-    url: ' https://redux.js.org/ ', 
-    author: ' Dan Abramov, Andrew Clark', 
-    num_comments: 2, 
-    points: 5, 
-    objectID: 1
-  }
-] 
-
-const List = ()=>(
+const List = (props)=>{
+  console.log('List rendered')
+  return(
       <ul>
-        {list.map(function (item){
-          return <li key = {item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>
+        {props.list.map(function (item){
+          return <Item item = {item}/>
         })}
       </ul>
-  );
+)
+}
 
+const Item = (props)=> {
+  console.log('Item rendered')
+
+  return(
+  <li key = {props.item.objectID}>
+    <span><a href={props.item.url}>{props.item.title}</a></span>
+    <span> {props.item.author}</span>
+    <span> {props.item.num_comments}</span>
+    <span> {props.item.points}</span>
+  </li>
+)
+}
 
 const Search = ()=>{
-  const handleChange = (event) =>{
-    console.log(event)
 
-    console.log(event.target.value);
-    
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = (event) =>{
+    setSearchTerm(event.target.value);
   }
+
+  console.log('Search rendered')
+
   return(
     <div>
       <label htmlFor="search"> Search:</label>
-      <input type="text" id = 'search' onChange={handleChange}/>
+      <input type="text" id = 'search' onChange={handleChange} placeholder='Search for anything'/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>      
+
     </div>
   )
-}
+};
 
-const App = ()=>(
+const App = ()=>{
+  const stories = [
+    {
+      title: ' React ', 
+      url: ' https://react.dev/ ',
+      author: 'Jordan Walke ', 
+      num_comments: 3,
+      points: 4, 
+      objectID: 0, 
+    }, 
+    {title: ' Redux ', 
+      url: ' https://redux.js.org/ ', 
+      author: ' Dan Abramov, Andrew Clark', 
+      num_comments: 2, 
+      points: 5, 
+      objectID: 1
+    }
+  ] 
+
+  console.log('App rendered');
+  
+  
+
+  return(
     <div>
       <h1>{welcome.greeting} {welcome.title}</h1>
 
       <Search />
 
       <hr />
-      <List />
+      <List list = {stories}/>
     </div>
-
     
-  );
+  )
+};
 
 export default App;
