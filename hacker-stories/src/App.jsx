8b650f06afdeac80bbf473a1194a8ea7b141c852
bilegyr06 <ayodejavu2@gmail.com>
2025-10-10 14:59:04 +0100

    Conditional_rendering

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -91,16 +91,19 @@ const App = () =>{
   }
 
   const [stories, setStories] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isError, setIsError] = React.useState(false)
   React.useEffect(()=>{
-    getAsyncStories().then(result => setStories(result.data.stories))
-  }, [])
+    setIsLoading(true)
+    getAsyncStories().then(result => {
+      setStories(result.data.stories)
+      setIsLoading(false)
+    }).catch(() => setIsError(true))
+  },[])
 
-  
   const handleRemoveStory = (item) =>{
     const newStories = stories.filter((story)=>
     item.objectID !== story.objectID)
-    // console.log(`search story removed: ${newStories}`)
-
     setStories(newStories)
   }
   
@@ -118,7 +121,9 @@ const App = () =>{
       </InputWithLabel>
 
       <hr />
-      <List list = {searchedStories} onRemoveItem = {handleRemoveStory}/>
+      {isError && <p>...Seems like something went wrong...</p>}
+      {isLoading ? (<p>Loading...</p>):
+      (<List list = {searchedStories} onRemoveItem = {handleRemoveStory}/>)}
     </div>
     
   )
