1008f9180b84e1c2939e58edd4a206d70797f9b9
bilegyr06 <ayodejavu2@gmail.com>
2025-10-14 10:44:15 +0100

    React_Impossible_states

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -73,10 +73,10 @@ const initialStories = [
 ] 
 
 const getAsyncStories = () => (
-  new Promise((resolve) => 
-    setTimeout(
-      () => resolve({data: {stories: initialStories}}), 2000
-    )
+  new Promise((resolve, reject) => 
+    setTimeout(()=>{
+      resolve({data: {stories: initialStories}})
+    },2000)
   )
 )
 
@@ -86,12 +86,35 @@ const getAsyncStories = () => (
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
-  case 'SET_STORIES':
-    return action.payload;
+  case 'STORIES_FETCH_INIT':
+    return {
+      ...state,
+      isLoading: true,
+      isError: false
+    };
+
+  case 'STORIES_FETCH_SUCCESS':
+    return {
+      ...state,
+      isLoading: false,
+      isError: false,
+      data: action.payload
+    }
+  
+  case 'STORIES_FETCH_FAILURE':
+    return {
+      ...state,
+      isLoading: false,
+      isError: true
+    }
+  
   case 'REMOVE_STORY':
-    return state.filter(
+    return { 
+      ...state,
+      data: state.data.filter(
     (story) => action.payload.objectID !== story.objectID
-    );
+    )
+  };
   default:
     throw new Error();
   }
@@ -105,19 +128,22 @@ const App = () =>{
   }
 
   const [stories, dispatchStories] = React.useReducer(
-    storiesReducer, []
+    storiesReducer, {data: [], isLoading: false, isError: false}
   )
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [isError, setIsError] = React.useState(false)
+
   React.useEffect(()=>{
-    setIsLoading(true)
+    dispatchStories({
+      type: 'STORIES_FETCH_INIT'
+    })
     getAsyncStories().then(result => {
       dispatchStories({
-        type: 'SET_STORIES',
+        type: 'STORIES_FETCH_SUCCESS',
         payload: result.data.stories
       })
-      setIsLoading(false)
-    }).catch(() => setIsError(true))
+
+    }).catch(() => dispatchStories({
+      type: 'STORIES_FETCH_FAILURE'
+    }))
   },[])
 
   const handleRemoveStory = (item) =>{
@@ -127,7 +153,7 @@ const App = () =>{
     })
   }
   
-  const searchedStories = stories.filter((story)=>
+  const searchedStories = stories.data.filter((story)=>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   )
   // console.log(newStories);
@@ -141,8 +167,8 @@ const App = () =>{
       </InputWithLabel>
 
       <hr />
-      {isError && <p>...Seems like something went wrong...</p>}
-      {isLoading ? (<p>Loading...</p>):
+      {stories.isError && <p>...Seems like something went wrong...</p>}
+      {stories.isLoading ? (<p>Loading...</p>):
       (<List list = {searchedStories} onRemoveItem = {handleRemoveStory}/>)}
     </div>
     
