9303c7e97498595a503b0da374803def5e8ddd4c
bilegyr06 <ayodejavu2@gmail.com>
2025-10-18 09:21:50 +0100

    Data fetching with react

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -54,31 +54,6 @@ const useStorageState = (key, initialState) =>{
   return [value, setValue]
 }
 
-const initialStories = [
-  {
-    title: ' React ',
-    url: ' https://react.dev/ ',
-    author: 'Jordan Walke ', 
-    num_comments: 3,
-    points: 4, 
-    objectID: 0, 
-  }, 
-  {title: ' Redux ', 
-    url: ' https://redux.js.org/ ', 
-    author: ' Dan Abramov, Andrew Clark', 
-    num_comments: 2, 
-    points: 5, 
-    objectID: 1
-  }
-] 
-
-const getAsyncStories = () => (
-  new Promise((resolve, reject) => 
-    setTimeout(()=>{
-      resolve({data: {stories: initialStories}})
-    },2000)
-  )
-)
 
 // React.useEffect(()=>{
 //   new Promise((resolve) => resolve({data: {stories: initialStories}}))
@@ -120,6 +95,7 @@ const storiesReducer = (state, action) => {
   }
 }
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=%27'
 
 const App = () =>{
   const [searchTerm, setSearchTerm] = useStorageState('search','React')
@@ -135,15 +111,17 @@ const App = () =>{
     dispatchStories({
       type: 'STORIES_FETCH_INIT'
     })
-    getAsyncStories().then(result => {
+    fetch(`${API_ENDPOINT}react`)
+    .then((res)=>
+      res.json()
+    )
+    .then((result)=>{
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories
+        payload: result.hits
       })
-
-    }).catch(() => dispatchStories({
-      type: 'STORIES_FETCH_FAILURE'
-    }))
+    })
+    .catch(()=> dispatchStories({type: 'STORIES_FETCH_FAILURE'}))
   },[])
 
   const handleRemoveStory = (item) =>{
