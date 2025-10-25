dc0cd1ce0ebb9608bb004fef70375afe94662239
bilegyr06 <ayodejavu2@gmail.com>
2025-10-25 18:38:06 +0100

    async/await in react

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -116,21 +116,22 @@ const App = () =>{
     storiesReducer, {data: [], isLoading: false, isError: false}
   )
 
-  const handleFetchStories = React.useCallback(()=>{
+  const handleFetchStories = React.useCallback(async ()=>{
     // no if (!searchTerm) return anymore because the button handles the emptyness of the search term
     dispatchStories({
       type: 'STORIES_FETCH_INIT'
     })
 
-    axios
-      .get(url)
-      .then((result)=>{
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits
-        })
+    try {
+      const result = await axios.get(url); 
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits
       })
-    .catch(()=> dispatchStories({type: 'STORIES_FETCH_FAILURE'}))
+    } catch {
+      dispatchStories({type: 'STORIES_FETCH_FAILURE'})
+    }
   },[url])
 
   React.useEffect(()=>{
